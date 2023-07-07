aa9d33c22283baa9389930ab5afa6334fea185d2
Szabo <92010495+MagentaPinguin@users.noreply.github.com>
2023-07-07 15:12:58 +0300

    7.7.2023

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dosar-transport" element={<DosarTransportPage />} />
