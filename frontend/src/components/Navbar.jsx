6be9c54f5fd2a6f4d8e9ead95d17eb770e897b16
MihaiPrb <porumbmihai411@gmail.com>
2023-07-07 09:30:06 +0300

    fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -67,23 +67,9 @@ export default function Navbar() {
                                 </div>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-<<<<<<< HEAD
-                                
-                            
-
-                            <Link to={'/login'}>
-                                    <Button text='Login'>
-
-                                    </Button>
-                            </Link>
-
-                          
-
-=======
                                 <Link to='/login'>
                                     <Button text='Login'/>
                                 </Link>
->>>>>>> main
 
                                 {/* Profile dropdown */}
                                 <Menu as="div" className="relative ml-3">
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -59,7 +59,7 @@ const RegisterPage = () => {
           className=" lg:w-3/4  border-gray-400 border-2 text-gray-900 outline-none text-sm rounded-md p-3 focus:border-sky-500"
           type="password"
           placeholder="Password"
-          required
+          requiredßå
           autoComplete="off"
           onChange={(e) => setUserData({...userData, password: e.target.value})}
         />
