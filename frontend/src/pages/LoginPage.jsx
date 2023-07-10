b440005f8a63e7f182b58b99499a1cfde859587c
alex.abrudan <alexandruabrudan0@gmail.com>
2023-07-10 11:46:35 +0300

    login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,39 +1,72 @@
-import { Link } from "react-router-dom"
-import Button from "../components/Button"
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import { useState, useEffect } from "react";
+import Input from "../components/Input";
 
-const Base_Url = "http://localhost:80/api";
+const BASE_URL = "http://localhost:80/api";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [Password, setPassword] = useState("");
+  const onChange = (field, value) => {
+    setUserData({ ...userData, [field]: value });
+  };
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${BASE_URL}/loginUser`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      const responseText = await response.text();
+      if (response.status === 200) {
+        console.log("success");
+        alert("Login reusit... navigare pe pagina HOME");
+        navigate("/");
+      } else {
+        setError(responseText);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-    <div className="flex w-full justify-center p-4"> 
-        <form className="flex flex-col items-center justify-around border-gray-200 border-2 rouded-md w-3/4 lg:w"> 
-            <input 
-              className="lg:3/4 "
-              type="text" required placeholder="email" autoCompleter="off" />
-            <input 
-              type="Password"
-              placeholder="Password" 
-              required
-              autoCompleter="off"
-            />
-            <Button text="Login" />
-            <div className="flex text-lg">
-                <p>Nu ai cont?</p>
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
 
-            
-            </div>
-
-        </form>
-    </div>;
-    );
+  return (
+    <div className="flex w-full justify-center p-4">
+      <form className="flex flex-col items-center justify-around border w-3/4 h-96 px-2">
+        <Input
+          placeholder="email"
+          name="email"
+          type="email"
+          onChange={(e) => onChange(e.target.name, e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          name="password"
+          type="password"
+          onChange={(e) => onChange(e.target.name, e.target.value)}
+        />
+        <Button text="Login" onClick={handleSubmit} />
+        <div className="flex text-lg">
+          <p>Nu ai cont?</p>
+          <Link className="ml-2 underline text-blue-500" to="/register">
+            Înregistrează-te
+          </Link>
+        </div>
+        {error ? <div className="text-red-600">{error}</div> : null}
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
