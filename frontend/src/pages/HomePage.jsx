20f3bb0cb48b88d05bb82ed5a73c6b3ae6870773
MihaiPrb <porumbmihai411@gmail.com>
2023-07-10 22:41:15 +0300

    fix

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,9 @@
-import { useEffect } from "react";
 import Scanner from "../components/Scanner";
-import sql from "../services/SqlService";
 
 export default function HomePage() {
-  useEffect(() => {
-    sql.query('test');
-  }, []);
   return (
-    <div>
-      <Link to="/dosar-transport">Navigheaza la dosar transport</Link>
+    <body>
       <Scanner />
-    </div>
+    </body>
   );
 }
