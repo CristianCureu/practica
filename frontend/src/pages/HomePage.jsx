74019b48696b5914095d6d67c2268d6ae736d0fa
denniissa <131274031+denniissa@users.noreply.github.com>
2023-07-13 12:03:22 +0300

    .

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import Scanner from "../components/Scanner";
-
-export default function HomePage() {
-=======
 import { useEffect } from "react";
 import Scanner from "../components/Scanner";
 
 export default function HomePage() {
 
->>>>>>> 150672eb5f561d04eac58bb820ee938d8c79c6ce
   return (
     <div>
       
