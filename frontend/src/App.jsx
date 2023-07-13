eb53f3d356f6462a94f35f38ea19358e2fbabca0
antoniolah <antoniolah00@gmail.com>
2023-07-13 12:33:23 +0300

    d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DosarTransportPage from "./pages/DosarTransportPage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 import StatusPage from "./pages/StatusPage";
 import Users from "./pages/Users";
-=========
->>>>>>>>> Temporary merge branch 2
+
 
 function App() {
   return (
@@ -19,7 +20,9 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/users" element={<Users />} />
-=========
+          <Route path="/status" element={<StatusPage />} />
+
+
         </Routes>
       </Router>
     </div>
