f515b898efe1e43a916f68a36a2319c18cb24368
Gabi273 <96780484+Gabi273@users.noreply.github.com>
2023-07-14 08:54:26 +0300

    ceva

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Sidebar() {
   return (
     <div>
-        <aside className='bg-gray-800 w-48 h-screen'>
+        <aside className='bg-gray-800 w-48 h-full'>
             <ul className="text-white pt-6 pl-10 flex flex-col gap-6 ">
                 <li><a href="/Dashboard" className='rounded-lg p-2 hover:bg-gray-700 '>Dashboard</a></li>
                 <li><a href="/Dosare" className='rounded-lg p-2 hover:bg-gray-700 '>Dosare</a></li>
